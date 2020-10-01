9ff57d88279663fd735c87ba45fa1d0696aea58f
ahbohhh <71724091+ahbohhh@users.noreply.github.com>
2020-10-01 14:41:10 +0200

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Usa il tuo Userbot,non il mio!"
+            reply_pop_up_alert = "we fratm perchè cerchi di usare il mio ubot?"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
@@ -55,7 +55,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Usa il tuo Userbot,non il mio!"
+            reply_pop_up_alert = "we fratm perchè cerchi di usare il mio ubot?"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
     @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
@@ -70,7 +70,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         except:
             pass
         if help_string is "":
-            reply_pop_up_alert = "{} is useless".format(plugin_name)
+            reply_pop_up_alert = "il creatore di {} è scemo e non ha messo la lista comandi".format(plugin_name)
         else:
             reply_pop_up_alert = help_string
         reply_pop_up_alert += "\n Usa .unload {} per rimuovere il plugin\n\
