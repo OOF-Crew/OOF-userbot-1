1ef12e8e25e89bcf6c2e9180e2f232abd4a2f321
ahbohhh <71724091+ahbohhh@users.noreply.github.com>
2020-10-01 14:33:05 +0200

    Update __main__.py
    cambiate alcune info heroku

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -60,7 +60,7 @@ for name in files:
 
 import userbot._core
 
-print("USERBOT-100101110 in esecuzione, test con .alive")
+print("OOF-userbot avviato, test con .alive")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
